221daa376e86f051067e3787348c3946f9c87243
MAITAYORMARIS <yormarism@gmail.com>
2023-06-15 22:20:02 -0300

    ensamblaje

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -15,7 +15,11 @@ import HongKong from '../images/HongKong.jpg';
 import Rio from '../images/RioDeJaneiro.jpg';
 import Mexico from '../images/ciudadDeMexicojpg.jpg';
 import Margarita from '../images/Margaritas.jpg';
-import BA from '../images/buenosAires.jpg'
+import BA from '../images/buenosAires.jpg';
+import Ibiza from '../images/ibiza.jpg';
+import Madrid from '../images/madrid.jpg';
+import Munich from '../images/munich.jpg';
+import Paris from '../images/paris.jpg';
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from 'swiper';
@@ -29,124 +33,130 @@ export default function Carousel() {
     };
     return (
         <>
-            <Swiper
-                spaceBetween={30}
-                centeredSlides={true}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={true}
-                modules={[Autoplay, Pagination, Navigation]}
-                onAutoplayTimeLeft={onAutoplayTimeLeft}
-                className="mySwiper"
-            >
-                <div>Titulo del Carousel</div>
-                <SwiperSlide>
-                    <div className='slide-item'>
-                        <div className="geometricA"></div>
-                        <div className="geometricB"></div>
-                        <div className="title-slide-item">Rio de Janeiro</div>
-                        <div className="subtitle-slide-item">Brasil</div>
-                        <img alt="Redentor Crist" src={Rio} />
-                    </div>
-                    <div className='slide-item'>
-                        <div className="geometricA"></div>
-                        <div className="geometricB"></div>
+            <div className='carouselContain'>
+                <div className='carouselTittle'>Popular MYtineraries</div>
+                <Swiper
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    onAutoplayTimeLeft={onAutoplayTimeLeft}
+                    className="mySwiper"
+                >
+                    <div>Titulo del Carousel</div>
+                    <SwiperSlide>
+                        <div className='slide-item'>
+                            <div className="geometricA"></div>
+                            <div className="geometricB"></div>
+                            <div className="title-slide-item">Rio de Janeiro</div>
+                            <div className="subtitle-slide-item">Brasil</div>
+                            <img alt="Redentor Crist" src={Rio} />
+                        </div>
+                        <div className='slide-item'>
+                            <div className="geometricA"></div>
+                            <div className="geometricB"></div>
 
-                        <div className="title-slide-item">Mexico City</div>
-                        <div className="subtitle-slide-item">Mexico</div>
-                        <img alt="Mexico City" src={Mexico} />
-                    </div>
-                    <div className='slide-item'>
-                        <div className="geometricA"></div>
-                        <div className="geometricB"></div>
-                        <div className="title-slide-item">Margarita Island</div>
-                        <div className="subtitle-slide-item">Venezuela</div>
-                        <img alt="Margarita Island Venezuela" src={Margarita} />
-                    </div>
-                    <div className='slide-item'>
-                        <div className="geometricA"></div>
-                        <div className="geometricB"></div>
-                        <div className="title-slide-item">Buenos Aires</div>
-                        <div className="subtitle-slide-item">Argentina</div>
-                        <img alt="Madero Port Buenos Aires" src={BA} />
-                    </div>
-                </SwiperSlide>
+                            <div className="title-slide-item">Mexico City</div>
+                            <div className="subtitle-slide-item">Mexico</div>
+                            <img alt="Mexico City" src={Mexico} />
+                        </div>
+                        <div className='slide-item'>
+                            <div className="geometricA"></div>
+                            <div className="geometricB"></div>
+                            <div className="title-slide-item">The Asuntion</div>
+                            <div className="subtitle-slide-item">Venezuela</div>
+                            <img alt="Margarita Island Venezuela" src={Margarita} />
+                        </div>
+                        <div className='slide-item'>
+                            <div className="geometricA"></div>
+                            <div className="geometricB"></div>
+                            <div className="title-slide-item">Buenos Aires</div>
+                            <div className="subtitle-slide-item">Argentina</div>
+                            <img alt="Madero Port Buenos Aires" src={BA} />
+                        </div>
+                    </SwiperSlide>
 
-                <SwiperSlide>
-                    <div className='slide-item'>
-                        <div className="geometricA"></div>
-                        <div className="geometricB"></div>
-                        <div className="title-slide-item">Singapur</div>
-                        <div className="subtitle-slide-item">Singapur</div>
-                        <img alt="Singapur" src={Singapur} />
-                    </div>
-                    <div className='slide-item'>
-                        <div className="geometricA"></div>
-                        <div className="geometricB"></div>
-                        <div className="title-slide-item">Male</div>
-                        <div className="subtitle-slide-item">Maldives Islands</div>
-                        <img alt="Maldives Islands" src={Maldivas} />
-                    </div>
-                    <div className='slide-item'>
-                        <div className="geometricA"></div>
-                        <div className="geometricB"></div>
-                        <div className="title-slide-item">Hong Kong</div>
-                        <div className="subtitle-slide-item">China</div>
-                        <img alt="Hong Kong" src={HongKong} />
-                    </div>
-                    <div className='slide-item'>
-                        <div className="geometricA"></div>
-                        <div className="geometricB"></div>
-                        <div className="title-slide-item">Agra</div>
-                        <div className="subtitle-slide-item">India</div>
-                        <img alt="Thag Majal" src={India} />
-                    </div>
-                </SwiperSlide>
+                    <SwiperSlide>
+                        <div className='slide-item'>
+                            <div className="geometricA"></div>
+                            <div className="geometricB"></div>
+                            <div className="title-slide-item">Singapur</div>
+                            <div className="subtitle-slide-item">Singapur</div>
+                            <img alt="Singapur" src={Singapur} />
+                        </div>
+                        <div className='slide-item'>
+                            <div className="geometricA"></div>
+                            <div className="geometricB"></div>
+                            <div className="title-slide-item">Male</div>
+                            <div className="subtitle-slide-item">Maldives Islands</div>
+                            <img alt="Maldives Islands" src={Maldivas} />
+                        </div>
+                        <div className='slide-item'>
+                            <div className="geometricA"></div>
+                            <div className="geometricB"></div>
+                            <div className="title-slide-item">Hong Kong</div>
+                            <div className="subtitle-slide-item">China</div>
+                            <img alt="Hong Kong" src={HongKong} />
+                        </div>
+                        <div className='slide-item'>
+                            <div className="geometricA"></div>
+                            <div className="geometricB"></div>
+                            <div className="title-slide-item">Agra</div>
+                            <div className="subtitle-slide-item">India</div>
+                            <img alt="Thag Majal" src={India} />
+                        </div>
+                    </SwiperSlide>
 
-                <SwiperSlide>
-                    <div className='slide-item'>
-                        <div className="geometricA"></div>
-                        <div className="geometricB"></div>
-                        <div className="title-slide-item">Ciudad</div>
-                        <div className="subtitle-slide-item">Pais</div>
-                        <img alt="imagen Singapur" src={Singapur} />
-                    </div>
-                    <div className='slide-item'>
-                        <div className="geometricA"></div>
-                        <div className="geometricB"></div>
-                        <div className="title-slide-item">Ciudad</div>
-                        <div className="subtitle-slide-item">Pais</div>
-                        <img alt="imagen Singapur" src={Singapur} />
-                    </div>
-                    <div className='slide-item'>
-                        <div className="geometricA"></div>
-                        <div className="geometricB"></div>
-                        <div className="title-slide-item">Ciudad</div>
-                        <div className="subtitle-slide-item">Pais</div>
-                        <img alt="imagen Singapur" src={Singapur} />
-                    </div>
-                    <div className='slide-item'>
-                        <div className="geometricA"></div>
-                        <div className="geometricB"></div>
-                        <div className="title-slide-item">Ciudad</div>
-                        <div className="subtitle-slide-item">Pais</div>
-                        <img alt="imagen Singapur" src={Singapur} />
-                    </div>
-                </SwiperSlide>
+                    <SwiperSlide>
+                        <div className='slide-item'>
+                            <div className="geometricA"></div>
+                            <div className="geometricB"></div>
+                            <div className="title-slide-item">Madrid</div>
+                            <div className="subtitle-slide-item">Espain</div>
+                            <img alt="imagen Madrid" src={Madrid} />
+                        </div>
+                        <div className='slide-item'>
+                            <div className="geometricA"></div>
+                            <div className="geometricB"></div>
+                            <div className="title-slide-item">Munich</div>
+                            <div className="subtitle-slide-item">Germany</div>
+                            <img alt="imagen Munich" src={Munich} />
+                        </div>
+                        <div className='slide-item'>
+                            <div className="geometricA"></div>
+                            <div className="geometricB"></div>
+                            <div className="title-slide-item">Ibiza</div>
+                            <div className="subtitle-slide-item">Espain</div>
+                            <img alt="imagen Ibiza" src={Ibiza} />
+                        </div>
+                        <div className='slide-item'>
+                            <div className="geometricA"></div>
+                            <div className="geometricB"></div>
+                            <div className="title-slide-item">Paris</div>
+                            <div className="subtitle-slide-item">France</div>
+                            <img alt="imagen Paris" src={Paris} />
+                        </div>
+                    </SwiperSlide>
 
 
-                <div className="autoplay-progress" slot="container-end">
-                    <svg viewBox="0 0 48 48" ref={progressCircle}>
-                        <circle cx="24" cy="24" r="20"></circle>
-                    </svg>
-                    <span ref={progressContent}></span>
-                </div>
-            </Swiper>
+                    <div className="autoplay-progress" slot="container-end">
+                        <svg viewBox="0 0 48 48" ref={progressCircle}>
+                            <circle cx="24" cy="24" r="20"></circle>
+                        </svg>
+                        <span ref={progressContent}></span>
+                    </div>
+                </Swiper>
+
+
+            </div>
+
         </>
     );
 }
--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,18 +1,16 @@
 import '../styles/welcome.css';
 import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
+
 import { Link } from 'react-router-dom';
 import Banner from '../images/banner Hero.png'
 
-function Welcome(){
-return (
+function Welcome() {
+  return (
     <div className="welcome">
       <img src={Banner} alt="home"></img>
-        <Button className='call' as={Link} to="/cities" variant="contained" endIcon={<SendIcon />}>
-        Here We Go!
+      <Button className='call' as={Link} to="/cities"> Here We Go!
       </Button>
-     
     </div>
-)
+  )
 }
 export default Welcome;
